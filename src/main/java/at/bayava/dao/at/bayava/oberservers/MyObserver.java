727a70501d42e2e530bea3b07b390f1f784e25dd
Philipp Bayer <p.bayer@freemail.hu>
2014-10-12 10:54:15 +0200

    Javadoc

--- src/main/java/at/bayava/dao/at/bayava/oberservers/MyObserver.java
+++ src/main/java/at/bayava/dao/at/bayava/oberservers/MyObserver.java
@@ -20,18 +20,42 @@ public class MyObserver {
 
 	String observedJohnMyNamedEvent;
 
+	/**
+	 * This method has no qualifier. Thus it should receive all string events(Event<String>), even those with qualifier.
+	 *
+	 * @param event the observed event
+	 */
 	void observeString(@Observes String event) {
 		observedEvent = event;
 	}
 
+	/**
+	 * This method has the @Named qualifier. Only string events where the @Named value is "Anne" will be received.
+	 *
+	 * @param event the observed event
+	 */
 	void observesAnneNamedString(@Observes @Named("Anne") String event) {
 		observedAnneEvent = event;
 	}
 
+	/**
+	 * This method has the @Named qualifier. Only string events where the @Named value is "John" will be received.
+	 *
+	 * @param event the observed event
+	 */
 	void observesJohnNamedString(@Observes @Named("John") String event) {
 		observedJohnEvent = event;
 	}
 
+	/**
+	 * This method has the @Named and @MyNamed qualifier. Only string events where the @Named value is "John" will be received.
+	 * Note, that the @MyNamed annotation must be present on the firing event, but its value doesnt matter. This is because the
+	 * value of @MyNamed annotation is marked with @Nonbinding.
+	 * Events received by this method should also be received by the observesJohnNamedString method, since it also has a matching
+	 * {@literal @}Named qualifier.
+	 *
+	 * @param event the observed event
+	 */
 	void observesJohnAnneNamedString(@Observes @Named("John") @MyNamed("xxx") String event) {
 		observedJohnMyNamedEvent = event;
 	}
--- src/test/java/oberservers/MyObserverTest.java
+++ src/test/java/oberservers/MyObserverTest.java
@@ -33,42 +33,73 @@ public class MyObserverTest {
 
 	@Inject
 	@Named("John")
+	//note how the value is different from the observer method
 	@MyNamed("whatverIdontCare")
 	Event<String> johnMyNamedEvent;
 
+	/**
+	 * Tests if only the appropriate methods received the event.
+	 *
+	 * @throws Exception
+	 */
 	@Test
 	public void testObserveString() throws Exception {
 		unnamedEvent.fire("unnamedEvent");
+		//Default receiver for string events, should always be fired
 		assertThat(myObserver.getObservedEvent(), is("unnamedEvent"));
+		//no other event should have been fired
 		assertThat(myObserver.getObservedAnneEvent(), nullValue());
 		assertThat(myObserver.getObservedJohnEvent(), nullValue());
 		assertThat(myObserver.getObservedJohnMyNamedEvent(), nullValue());
 	}
 
+	/**
+	 * Tests if only the appropriate methods received the event.
+	 *
+	 * @throws Exception
+	 */
 	@Test
 	public void testObservesAnneNamedString() throws Exception {
 		anneEvent.fire("anneEvent");
+		//Default receiver for string events, should always be fired
 		assertThat(myObserver.getObservedEvent(), is("anneEvent"));
 		assertThat(myObserver.getObservedAnneEvent(), is("anneEvent"));
+		//no other event should have been fired
 		assertThat(myObserver.getObservedJohnEvent(), nullValue());
 		assertThat(myObserver.getObservedJohnMyNamedEvent(), nullValue());
 	}
 
+	/**
+	 * Tests if only the appropriate methods received the event.
+	 *
+	 * @throws Exception
+	 */
 	@Test
 	public void testObservesJohnNamedString() throws Exception {
 		johnEvent.fire("johnEvent");
+		//Default receiver for string events, should always be fired
 		assertThat(myObserver.getObservedEvent(), is("johnEvent"));
-		assertThat(myObserver.getObservedAnneEvent(), nullValue());
 		assertThat(myObserver.getObservedJohnEvent(), is("johnEvent"));
+		//no other event should have been fired
+		assertThat(myObserver.getObservedAnneEvent(), nullValue());
 		assertThat(myObserver.getObservedJohnMyNamedEvent(), nullValue());
 	}
 
+	/**
+	 * Tests if only the appropriate methods received the event.
+	 *
+	 * @throws Exception
+	 */
 	@Test
 	public void testObservesJohnMyNamedString() throws Exception {
 		johnMyNamedEvent.fire("johnMyNamedEvent");
+		//Default receiver for string events, should always be fired
 		assertThat(myObserver.getObservedEvent(), is("johnMyNamedEvent"));
-		assertThat(myObserver.getObservedAnneEvent(), nullValue());
-		assertThat(myObserver.getObservedJohnEvent(), is("johnMyNamedEvent"));
+		//test if the event was fired, even though the @MyNamed value doesnt match
 		assertThat(myObserver.getObservedJohnMyNamedEvent(), is("johnMyNamedEvent"));
+		//John event should also have been fired, since the @Named value matches
+		assertThat(myObserver.getObservedJohnEvent(), is("johnMyNamedEvent"));
+		//no other event should have been fired
+		assertThat(myObserver.getObservedAnneEvent(), nullValue());
 	}
 }
