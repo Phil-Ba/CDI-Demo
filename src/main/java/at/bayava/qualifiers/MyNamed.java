7dca3141fbc4c072d1c392029d73d6f99c090119
Philipp Bayer <p.bayer@freemail.hu>
2014-10-12 11:10:16 +0200

    javadoc

--- src/main/java/at/bayava/qualifiers/MyNamed.java
+++ src/main/java/at/bayava/qualifiers/MyNamed.java
@@ -11,5 +11,12 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 @Qualifier
 public @interface MyNamed {
+
+	/**
+	 * value is nonbinding, so it will be ignored when determining injection point.
+	 * This is useful if you want to pass a value (ie. to an interceptor)
+	 *
+	 * @return
+	 */
 	@Nonbinding String value() default "";
 }
